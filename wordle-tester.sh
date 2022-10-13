91795ed8bb2d5e1f9b8bc590e3fa1b9272af8884
raul saavedra <raul.saavedra@gmail.com>
2022-10-13 20:44:31 +0200

    Two new tests added, after the Spanish wordle from today and realizing more filtering was possible for black letters that had also been previously seen as yellows or greens

--- wordle-tester.sh
+++ wordle-tester.sh
@@ -103,4 +103,34 @@ else
     good "05"
 fi
 
+# Test case created on 13.10.2022, after the Spanish wordle that day:
+# If a letter in green (or yellow) also appears now in black,
+# then any word with one or more too many repetitions of that
+# letter can already be discarded. Examples:
+# Guess 'impio' resulted in ---gg as clues, so any words that has
+# the letter i repeated (e.g. tibio) could and should be discarded.
+# After that, tried folio, an 'o' was still green but the
+# additional 'o' was black. Same thing here then: any words
+# with a repeated 'o' (e.g. gofio, obvio) should and could also be
+# discarded after these last clues.
+# (Final solution was junio)
+LASTWORDSET=`$RUNWH -s -binput_test_es_03.txt | grep "Actual words" | tail -n 1`
+MATCHCOUNT=`echo $LASTWORDSET | grep -v "litio" | grep -v "obvio" | grep "junio" | wc -l`
+if [[ "$MATCHCOUNT" != "1" ]]; then
+    failed "06"
+else
+    good "06"
+fi
+
+# Additional test case, to check simple removal of black letters
+# After guess/clues aside/y---y, faker/-y-y-, final list cannot
+# contain words like zebra (because r was black)
+LASTWORDSET=`$RUNWH -binput_test_en_04.txt -n500 | grep "Actual words" | tail -n 1`
+MATCHCOUNT=`echo $LASTWORDSET | grep -v "zebra" | grep "equal" | wc -l`
+if [[ "$MATCHCOUNT" != "1" ]]; then
+    failed "07"
+else
+    good "07"
+fi
+
 exit $RESULT
